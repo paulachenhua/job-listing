ff4a534a41caf3fe107fb560c363effad6ebb3a1
paulachenhua <chenhua1718@gmail.com>
2017-04-19 17:15:57 +0800

    纠错

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -12,9 +12,9 @@ before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destro
    def index
     @jobs = case params[:order]
             when 'by_lower_bound'
-              Job.published.order.order('wage_lower_bound DESC')
+              Job.published.order('wage_lower_bound DESC')
             when 'by_upper_bound'
-              Job.published.order.order('wage_upper_bound DESC')
+              Job.published.order('wage_upper_bound DESC')
             else
               Job.published.recent
             end
