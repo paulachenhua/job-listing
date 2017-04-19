8df683489daf17b239703387a1ef2a697be63104
paulachenhua <chenhua1718@gmail.com>
2017-04-19 16:02:26 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,10 @@ Rails.application.routes.draw do
 
  namespace :admin do
   resources :jobs do
-
       member do
         post :publish
         post :hide
       end
-
-
       resources :resumes
     end
   end
